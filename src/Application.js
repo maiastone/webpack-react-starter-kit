518cb1ee05d9a77a1551ab0c679c827f02be3da5
Maia Stone <maiadov@gmail.com>
2016-12-23 11:44:46 -0700

    Add react

---
+++ src/Application.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Application extends React.Component {
+
+
+  render() {
+    return (
+      <h1>Webpack-React-Starter-Kit</h1>
+    );
+  }
+}
+
+module.exports = Application;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('Lo!');
+const React = require('react');
+const ReactDOM = require('react-dom');
+import Application from './Application.js'
+
+
+
+ReactDOM.render(<Application />, document.getElementById('application'));
